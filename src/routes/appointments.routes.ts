d12db2d12edd260ba9d6c522f8ae228d923a0f38
DESKTOP-6P7GRMM\fukud <fukudaum@gmail.com>
2023-02-28 00:46:47 -0300

    init commit Starting goBarber again!

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,30 @@
+import { Request, Response, Router } from "express";
+import { startOfHour, parseISO, isEqual } from "date-fns";
+import Appointment from "../models/Appointment";
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request: Request, response: Response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find( appointment => {
+        isEqual(parsedDate, appointment.date);
+    });
+
+    if(findAppointmentInSameDate) {
+        return response.status(400).json({
+            message: 'This appointment is already booked!'
+        });
+    }
+
+    const appointment = new Appointment(provider, parsedDate);
+
+    appointments.push(appointment);
+    return response.json(appointment);
+})
+
+export default appointmentsRouter;
